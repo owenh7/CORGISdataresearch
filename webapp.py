23c40298cfbf7e46a33c161f94d7fb2446af3cfa
owenh7 <77691097+owenh7@users.noreply.github.com>
2021-05-04 09:09:22 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -9,16 +9,21 @@ def render_main():
     print("RunningMain")
     with open('medal_of_honor.json') as medalofhonor_data:
         names = json.load(medalofhonor_data)
+        
     return render_template('page1.html')
 @app.route("/p1")
+
 def render_first():
     return render_template('page1.html')
+
 @app.route("/p2")
 def render_first2():
     return render_template('page2.html')
+
 @app.route("/p3")
 def render_first3():
     return render_template('page3.html')
+
 @app.route("/p4")
 def render_first4():
     return render_template('page4.html')
