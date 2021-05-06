b6ff49bcbfa4d854799fc181a4d8fcdbfc42244c
owenh7 <77691097+owenh7@users.noreply.github.com>
2021-05-05 20:09:00 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -10,8 +10,12 @@ def render_main():
     print("RunningMain")
     with open('medal_of_honor.json') as medal_data:
         names = json.load(medal_data)
-        
-        
+    return render_template('page1.html')
+@app.route("/p1")
+def render_first():
+    return render_template('page1.html')
+@app.route("/p2")
+def render_first2():
     if 'names' in request.args:
         return render_template('main.html', years = get_year_options(names), average_age = average_age(get_name_year(request.args['names'],names), names), names = get_name_options(get_name_year(request.args['names'],names),names), names_age = get_name_age(request.args['names'],names))
     if 'years' in request.args:
@@ -65,12 +69,6 @@ def get_name_year(name, names):
         if data["Name"] == name:
             year = data["Year"]
     return year
-    return render_template('page1.html')
-@app.route("/p1")
-def render_first():
-    return render_template('page1.html')
-@app.route("/p2")
-def render_first2():
     return render_template('page2.html')
 @app.route("/p3")
 def render_first3():
