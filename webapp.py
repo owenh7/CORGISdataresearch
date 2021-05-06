83a7761f3e1670392cb7b9d1a32235be8129c733
owenh7 <77691097+owenh7@users.noreply.github.com>
2021-05-05 20:43:58 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -29,8 +29,8 @@ def get_year_options(names):
     years = []
     print("RunningOP")
     for data in names:
-        if data["year"] not in years:
-            years.append(data["Year"])
+        if data["birth.date.year"] not in years:
+            years.append(data["birth.date.year"])
     options = ""
     for data in years:
         options = options + Markup("<option value=\"" + data + "\">" + data + "</option>")
@@ -41,7 +41,7 @@ def average_age(year, names):
     points = float(0)
     total = float(0)
     for name in names:
-        if name["year"] == year:
+        if name["birth.date.year"] == year:
             total = total + name["Age"]["Percent Under 18 Years"]
             points=points + 1
     avg = float(total//points)
@@ -51,7 +51,7 @@ def get_name_options(years,names):
     namelist = []
     print("RunningCOP")
     for name in names:
-        if name["year"] == years :
+        if name["birth.date.year"] == years :
             namelist.append(name["Name"])
     options = ""
     for data in namelist:
@@ -69,7 +69,7 @@ def get_name_year(name, names):
     year = ""
     for data in names:
         if data["Name"] == name:
-            year = data["year"]
+            year = data["birth.date.year"]
     return year
     return render_template('page2.html')
 @app.route("/p3")
