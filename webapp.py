0a729a5585e6ea9e3e7fbefc3c67c078b01237e7
owenh7 <77691097+owenh7@users.noreply.github.com>
2021-05-26 20:10:18 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -52,7 +52,7 @@ def get_issued_options(states,counties):
             issuedlist.append(["issued"])
     options = ""
     for data in issuedlist:
-        str(awarded_citation)
+        str(issued)
         options = options + Markup("<option value=\"" + data + "\">" + data + "</option>")
     return options
     
