7ee7da71e1f8fb803176e37ccb1e4e6d1bd29fd7
owenh7 <77691097+owenh7@users.noreply.github.com>
2021-05-27 09:57:32 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -70,6 +70,59 @@ def get_issued_state(issued, counties):
     return render_template('page2.html')
 @app.route("/p3")
 def render_first3():
+    with open('medal_of_honor.json') as medal_data:
+        counties = json.load(medal_data)
+    if 'counties' in request.args:
+        return render_template('page2.html', states = get_state_options(counties), military record_organization = military record_organization(get_issued_state(request.args['counties'],counties), counties), counties = get_issued_options(get_issued_state(request.args['counties'],counties),counties), issued_military record = get_issued_military record(request.args['counties'],counties))
+    if 'states' in request.args:
+        return render_template('page2.html', states = get_state_options(counties), military record_organization = military record_organization(request.args['states'], counties), counties = get_issued_options(request.args['states'],counties))
+    elif 'states' not in request.args and 'counties' not in request.args:
+        return render_template('page2.html', states = get_state_options(counties))
+    
+
+def get_state_options(counties):
+    states = []
+    print("RunningOP")
+    for data in counties:
+        if data["name"] not in states:
+            states.append(data["name"])
+    options = ""
+    for data in states:
+        options = options + Markup("<option value=\"" + str(data) + "\">" + str(data) + "</option>")
+    return options
+
+def military record_organization(state, counties):
+    print("RunningAge")
+    points = float(0)
+    total = float(0)
+    for issued in counties:
+        if issued["name"] == state:
+            total = issued["military record"]["orginization"]
+    return total
+def get_issued_options(states,counties):
+    issuedlist = []
+    print("RunningCOP")
+    for issued in counties:
+        if issued["name"] == states :
+            issuedlist.append(["issued"])
+    options = ""
+    for data in issuedlist:
+        options = options + Markup("<option value=\"" + str(data) + "\">" + str(data) + "</option>")
+    return options
+    
+def get_issued_awarded(issued, counties):
+    print("RunningCAge")
+    for issued1 in counties:
+        if issued1["issued"] == issued:
+            return issued1["military record"]["orginization"]
+ 
+def get_issued_state(issued, counties):
+    print("RunningState")
+    state = ""
+    for data in counties:
+        if data["issued"] == issued:
+            state = data["name"]
+    return state
     return render_template('page3.html')
 @app.route("/p4")
 def render_first4():
