c013a18e1c58d57c2ef7496c08cd972304fb4d76
owenh7 <77691097+owenh7@users.noreply.github.com>
2021-05-27 23:29:23 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -73,9 +73,9 @@ def render_first3():
     with open('medal_of_honor.json') as medal_data:
         counties = json.load(medal_data)
     if 'counties' in request.args:
-        return render_template('page3.html', states = get_state_options(counties), military record.organization = military record.organization(get_issued_state(request.args['counties'],counties), counties), counties = get_issued_options(get_issued_state(request.args['counties'],counties),counties), issued_military record = get_issued_military record(request.args['counties'],counties))
+        return render_template('page3.html', states = get_state_options(counties), military record_organization = military record_organization(get_issued_state(request.args['counties'],counties), counties), counties = get_issued_options(get_issued_state(request.args['counties'],counties),counties), issued_military record = get_issued_military record(request.args['counties'],counties))
     if 'states' in request.args:
-        return render_template('page3.html', states = get_state_options(counties), military record.organization = military record.organization(request.args['states'], counties), counties = get_issued_options(request.args['states'],counties))
+        return render_template('page3.html', states = get_state_options(counties), military record_organization = military record_organization(request.args['states'], counties), counties = get_issued_options(request.args['states'],counties))
     elif 'states' not in request.args and 'counties' not in request.args:
         return render_template('page3.html', states = get_state_options(counties))
     
@@ -91,7 +91,7 @@ def get_state_options(counties):
         options = options + Markup("<option value=\"" + str(data) + "\">" + str(data) + "</option>")
     return options
 
-def military record.organization(state, counties):
+def military record_organization(state, counties):
     print("RunningAge")
     points = float(0)
     total = float(0)
